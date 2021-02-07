ca5df71ed74edde0aec16074917d6e46c0f6045b
hunter frields <hunter.frields.19@cnu.edu>
2021-02-06 22:48:15 -0500

    added other files back

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//#includes are ALWAYS at top of file
+#include <iostream>	//iostream is where std::cout and std::endl are defined
+#include "myfunc.h"
+
+using namespace std;
+int main()
+{
+	cout<<func()<<endl;
+	return 0;
+}
